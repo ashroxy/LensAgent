734f122600a33b777b47678d0491f1702b10cd65
ASHNIW <srm651446@gmail.com>
2026-09-02 00:13:23 +0530

    chore: remove patch scripts

--- patch2.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('lib/capture.js', 'utf8');
-content = content.replace('_onScreencastFrame(params) {', 'async _onScreencastFrame(params) {');
-content = content.replace('const buffer = this._base64ToArrayBuffer(data);', 'const res = await fetch(data:image/jpeg;base64,\);\n    const buffer = await res.arrayBuffer();');
-fs.writeFileSync('lib/capture.js', content);
-console.log('patched');
--- patch3.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('lib/capture.js', 'utf8');
-content = content.replace(/const res = await fetch\\(.*\\);/, 'const res = await fetch(\data:image/jpeg;base64,\\);');
-fs.writeFileSync('lib/capture.js', content);
-console.log('patched');
