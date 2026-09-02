33220cea5dec4f5acb2530eec54e93601de87c09
ASHNIW <srm651446@gmail.com>
2026-09-02 07:24:13 +0530

    chore: remove patch scripts

--- patch_agent.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('lib/agent-loop.js', 'utf8');
-content = content.replace(
-  'chrome.runtime.sendMessage({ type: BG_AGENT_STATUS, payload: this.getStatus() }).catch(() => {});',
-  'chrome.runtime.sendMessage({ type: BG_AGENT_STATUS, payload: { activeTabId: this.tabId, ...this.getStatus() } }).catch(() => {});'
-);
-fs.writeFileSync('lib/agent-loop.js', content);
--- patch_bg2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('background/service-worker.js', 'utf8');
-content = content.replace('function broadcastToPopup(type, payload) {', 'function broadcastToPopup(type, payload) {\n  payload = payload || {};\n  if (activeTabId) payload.activeTabId = activeTabId;');
-fs.writeFileSync('background/service-worker.js', content);
