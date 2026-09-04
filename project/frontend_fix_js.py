06869009673050bc458326f4397279059ef77c0b
ASHNIW <srm651446@gmail.com>
2026-09-04 17:54:29 +0530

    clean: remove temp fix scripts from project directory

--- project/frontend_fix_js.py
+++
@@ -1,7 +0,0 @@
-﻿with open('e:/SIH-171/frontend/app.js', 'r', encoding='utf8') as f:
-    js = f.read()
-
-js = js.replace('../lib/', './lib/')
-
-with open('e:/SIH-171/frontend/app.js', 'w', encoding='utf8') as f:
-    f.write(js)
